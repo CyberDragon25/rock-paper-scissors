43d55ced6e42497be18abd6806cac1255dceab82
Shubham <bhattacharya.shubham123@gmail.com>
2023-12-17 21:05:55 -0600

    cmpleted program

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        console.log(`Congratulations!! You WIN. ${playerSelection} beats ${computerSelection}`);
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else {
+        console.log("It's a tie");
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);

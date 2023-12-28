1a4ac8b9d0019aeb8d339a8f874bebfcb2d64cd8
Shubham <bhattacharya.shubham123@gmail.com>
2023-12-27 18:52:24 -0600

    Completed project

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let playerScore = 0;
+let computerScore = 0;
+
+// Function to get computer's choice
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+// Function to play a round and update scores
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
@@ -12,49 +17,76 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "rock")
     ) {
-        console.log(`Congratulations!! You WIN. ${playerSelection} beats ${computerSelection}`);
-        return 'player';
+        playerScore++;
+        return 'Congratulations!! You WIN.';
     } else if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "scissors" && playerSelection == "paper") ||
         (computerSelection == "paper" && playerSelection == "rock")
     ) {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        return 'computer';
+        computerScore++;
+        return 'You Lose!';
     } else {
-        console.log("It's a tie");
-        return 'tie';
+        return "It's a tie";
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+// Function to update scores and check for the winner
+function updateScore(roundResult) {
+    const playerPoints = document.getElementById('player-points');
+    const computerPoints = document.getElementById('computer-points');
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your choice: rock, paper, or scissors");
-        const computerSelection = getComputerChoice();
+    playerPoints.textContent = `Player Points: ${playerScore}`;
+    computerPoints.textContent = `Computer Points: ${computerScore}`;
 
-        const roundResult = playRound(playerSelection, computerSelection);
-
-        if (roundResult === 'player') {
-            playerScore++;
-        } else if (roundResult === 'computer') {
-            computerScore++;
-        }
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
     }
+}
 
-    // Display the final results
-    console.log(`Final Score - Player: ${playerScore}, Computer: ${computerScore}`);
-    
+// Function to end the game and display the winner
+function endGame() {
+    const resultsDiv = document.getElementById('results');
+
     if (playerScore > computerScore) {
-        console.log("You win the game!");
+        resultsDiv.textContent = 'You win the game!';
     } else if (playerScore < computerScore) {
-        console.log("You lose the game!");
+        resultsDiv.textContent = 'You lose the game!';
     } else {
-        console.log("The game is a tie!");
+        resultsDiv.textContent = "The game is a tie!";
     }
+
+    // Optional: You can reset the scores here if you want to play another round
+    // playerScore = 0;
+    // computerScore = 0;
 }
 
-// Call the game function to start the best-of-five game
-game();
+// Function to play the game when a button is clicked
+function playGame(playerSelection) {
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    // Use DOM manipulation to update the results div
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.textContent = roundResult;
+
+    // Update scores and check for the winner
+    updateScore(roundResult);
+}
+
+// Get HTML elements for buttons
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+// Add onClick methods to buttons
+rockButton.addEventListener('click', function () {
+    playGame('rock');
+});
+
+paperButton.addEventListener('click', function () {
+    playGame('paper');
+});
+
+scissorsButton.addEventListener('click', function () {
+    playGame('scissors');
+});
